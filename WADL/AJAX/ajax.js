f154bce31e5d48eb01e5adc376ad36dfaed07cbc
Devansh Mishra <115351387+dkmishra2407@users.noreply.github.com>
2025-02-18 19:09:29 +0530

    Commiting

---
+++ WADL/AJAX/ajax.js
@@ -0,0 +1,56 @@
+console.log("first")
+
+let fetchbtn = document.getElementById('fetch');
+fetchbtn.addEventListener('click',handler1);
+
+let backup = document.getElementById('backup');
+backup.addEventListener('click',handler2);
+
+// function handler1(){
+
+//     console.log("You have clicked the fetchbtn");
+//     const xhr = new XMLHttpRequest();
+
+//     xhr.open('GET','https://jsonplaceholder.typicode.com/posts',true);//true means fetch the data asynchronously
+
+//     //progress (optional)
+
+//     xhr.onprogress = function(){
+//         console.log("On progress");
+//     }
+//     xhr.onload = function(){
+//         console.log(this.response);
+//     }
+//     //you have tosend the request
+//     xhr.send();
+// }
+
+// post 
+function handler1(){
+
+    console.log("You have clicked the fetchbtn");
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('POST','https://jsonplaceholder.typicode.com/posts',true);//true means fetch the data asynchronously
+    xhr.getResponseHeader('Content-type','appliction/json')
+    //progress (optional)
+
+     const data = {
+        "userId": "2",
+        "id": "18",
+        "title": "voluptate et itaque vero tempora molestiae",
+        "body": "eveniet quo quis\nlaborum totam consequatur non dolor\nut et est repudiandae\nest voluptatem vel debitis et magnam"
+      }
+
+    xhr.onprogress = function(){
+        console.log("On progress");
+    }
+    xhr.onload = function(){
+        console.log(this.response);
+    }
+    //you have tosend the request
+    xhr.send(data);
+}
+function handler2(){
+    console.log("You have clicked the backupbtn");
+}
