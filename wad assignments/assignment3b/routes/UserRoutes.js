82cd31432af91198ff6698b2a0c312d1bb109f8a
Devansh Mishra <115351387+dkmishra2407@users.noreply.github.com>
2025-03-29 00:14:00 +0530

    Completed Exchange Routes

--- wad assignments/assignment3b/routes/UserRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const UserController = require('../controller/UserController')
-
-router.get('/users',UserController.get_all_user);
-router.post('/add',UserController.add_new_user);
-router.get('/:id', UserController.findById)
-
-module.exports = router;
